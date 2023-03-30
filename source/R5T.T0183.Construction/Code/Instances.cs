98dfded69b01ea4f148dea9fea443c12aed19be9
davidcoats <david@getsafetycone.com>
2023-03-30 12:24:51 -0700

    Initial commit.

---
+++ source/R5T.T0183.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.T0183.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/R5T.T0183/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0183
+{
+	/// <summary>
+	/// Time unit strong-types (milliseconds, seconds, etc.)
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
